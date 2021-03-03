048a17749883b14dc7d2a8bca8ca8991fb5a1d80
pranavsrathod <79948560+pranavsrathod@users.noreply.github.com>
2021-03-03 00:46:41 -0600

    Update JavaFXTemplate.java

--- JavaFXTemplate.java
+++ JavaFXTemplate.java
@@ -1,18 +1,31 @@
+import java.util.HashMap;
+
 import javafx.application.Application;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.Scene;
+import javafx.scene.text.Font; 
+import javafx.scene.text.FontPosture; 
+import javafx.scene.text.FontWeight;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class JavaFXTemplate extends Application {
 	private BorderPane borderPane;
+	EventHandler<ActionEvent> startButton, backBotton;
 	private Button b1;
+	HashMap<String, Scene> sceneMap;
+	private Button gamePlay;
+	private Button theme;
+	private Button options;
 	private VBox root;
+	private HBox menuBar;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -23,25 +36,55 @@ public class JavaFXTemplate extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setTitle("Welcome to JavaFX");
+		sceneMap = new HashMap<String,Scene>();
+		primaryStage.setTitle("Welcome to Connect 4");
+		
+		// ------------------------------------------------------------------------------------------------------------------
 		b1 = new Button("Start");
+		b1.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
+		Label label = new Label("-------------- !! WELCOME TO CONNECT 4 !! --------------");
+		label.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
 		b1.setPrefWidth(100);
-		root = new VBox(10, b1);
+		root = new VBox(10,label, b1);
 		/*
 		 add an image to the background
 		 */
 		borderPane = new BorderPane();
+		borderPane.setCenter(label);
 		borderPane.setCenter(b1);
-		EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent e) {
-				// calling the gameButton
-			}
-		};
-		// sets the action
-		b1.setOnAction(handler);
-		Scene scene = new Scene(borderPane, 500,500);
-		primaryStage.setScene(scene);
+		b1.setOnAction(e -> primaryStage.setScene(sceneMap.get("game")));
+		Scene welcome = new Scene(borderPane, 500,500);
+		// ------------------------------------------------------------------------------------------------------------------
+		
+		sceneMap.put("welcome", welcome);
+		sceneMap.put("game", gameScene());
+		
+		primaryStage.setScene(sceneMap.get("welcome"));
 		primaryStage.show();
 	}
+
+	public Scene gameScene() {
+		Label label = new Label("-------------- !! Game Started !! --------------");
+		label.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
+		gamePlay = new Button("Game Play");
+		//gamePlay.setPrefWidth(50);
+		gamePlay.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
+		
+		theme = new Button("Theme");
+		theme.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
+		//theme.setPrefWidth(50);
+		
+		options = new Button("Options");
+		options.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
+		//options.setPrefWidth(50);
+		menuBar = new HBox(150, gamePlay, theme, options);
+		
+		BorderPane pane = new BorderPane();
+		pane.setTop(menuBar);
+		pane.setCenter(label);
+		pane.setStyle("-fx-background-color: lightBlue;");
+		return new Scene(pane, 600, 600);
+	}
+	
 
 }
