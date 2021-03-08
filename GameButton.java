5618e6a0c9a35c76ba37c1fc1e04a5f955e0bcdb
Parth-09 <71151806+Parth-09@users.noreply.github.com>
2021-03-07 20:48:24 -0600

    Update GameButton.java

--- GameButton.java
+++ GameButton.java
@@ -1,10 +1,15 @@
 import javafx.scene.control.Button;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Font;
 public class GameButton extends Button{
-	
+	public int color;
+	public 
 	GameButton(){
 		super("");
 		this.setPrefSize(100, 100);
-		this.setStyle("-fx-background-color: Grey");
+		this.setStyle("-fx-background-color: Black");
+		this.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
 		
 	}
 }
