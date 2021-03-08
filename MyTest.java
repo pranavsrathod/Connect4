ea270da7cef391c76e533ac68d4f95068e426d07
Parth-09 <71151806+Parth-09@users.noreply.github.com>
2021-03-07 22:40:25 -0600

    Create MyTest.java

---
+++ MyTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.DisplayName;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MyTest {
+// Since all the functions and pieces of code which were used to build this program consisted
+// of some link or the other to the GUI i.e graphical user interface it was not possible for us to do
+// the JUnit5 tests like usual. Which is why the code was tested manually by us considering almost all of the
+// possibilities for the Connect4 program. Thus, JUnit5 tests were not used in this scenario.
+
+}
